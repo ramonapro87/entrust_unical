670092678fa9a1da518c0612c2ce26c188dcf985
Ramona <ramonaprocopio@gmail.com>
2024-07-03 11:10:25 +0200

    Update IDiagrams.java
    chart disable

--- entrust/src/edu/boun/edgecloudsim/simulationvisualizer/IDiagrams.java
+++ entrust/src/edu/boun/edgecloudsim/simulationvisualizer/IDiagrams.java
@@ -2,24 +2,25 @@ package edu.boun.edgecloudsim.simulationvisualizer;
 
 import edu.boun.edgecloudsim.utils.Coordinates;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public interface IDiagrams {
 
     default void generateEnergyCharts(List<Coordinates> coordinates, String scenarioName, String orchestretorPolicy) {
 
+        if(chartDisable())
+            return;
+
         Map<Integer, List<Coordinates>> coordinatesById = coordinates.stream()
                 .collect(Collectors.groupingBy(Coordinates::getId));
 
-        // todo, filter only for test, remove next line for all data
-      //  List<Integer> filterId = List.of(1, 2);
-    //    coordinatesById = coordinatesById.entrySet().stream()
-      //          .filter(entry -> filterId.contains(entry.getKey()))
-          //      .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        //todo, filter only for test, remove next line for all data
+        //List<Integer> filterId = List.of(1, 2);
+        //coordinatesById = coordinatesById.entrySet().stream()
+        //.filter(entry -> filterId.contains(entry.getKey()))
+        //.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         generateEnergyCharts(coordinatesById, scenarioName, orchestretorPolicy);
     }
 
@@ -27,6 +28,9 @@ public interface IDiagrams {
 
 
     default void generateMapChart(List<Coordinates> coordinates, String scenarioName, String orchestretorPolicy) {
+        if(chartDisable())
+            return;
+
         Map<Integer, List<Coordinates>> coordinatesById = coordinates.stream()
                 .collect(Collectors.groupingBy(Coordinates::getId));
 
@@ -37,6 +41,11 @@ public interface IDiagrams {
         generateMapChart(coordinatesById, scenarioName, orchestretorPolicy);
     }
     public void generateMapChart(Map<Integer, List<Coordinates>> coordinatesById, String scenarioName, String orchestretorPolicy);
+
+
+    default boolean chartDisable() {
+        return true;
+    }
 
 
 }
